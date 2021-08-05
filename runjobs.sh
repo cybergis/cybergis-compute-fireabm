2359f1334f3baabf1aeeaf8fafc2c2420674aea1
Becky <25420142+beckvalle@users.noreply.github.com>
2021-08-05 10:47:45 -0500

    Update runjobs.sh

--- runjobs.sh
+++ runjobs.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-module purge
-module add GNU
-module add GPU
-module use /data/cigi/common/cigi-modules
-module load anaconda3
-conda activate ox
+pip install ipython
 
 SEED=$(($param_start_value + $SLURM_PROCID))
 echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value, SEED: $SEED\n"
