f29a61a7fd43f3d95fc36b528126aebedfc96494
Becky <25420142+beckvalle@users.noreply.github.com>
2021-08-05 10:22:44 -0500

    Update runjobs.sh

--- runjobs.sh
+++ runjobs.sh
@@ -1,7 +1,4 @@
 #!/bin/tcsh
 
-echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value\n"
-
-[ ! -d "/$executable_folder/" ] && echo "Directory /$result_folder/ DOES NOT exist" 
-[ -d "/$executable_folder/" ] && echo "Directory /$result_folder/ DOES exist" 
-
+SEED=param_start_value+SLURM_PROCID
+echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value, SEED: $SEED\n"
