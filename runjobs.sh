8cde005d745a0ce959c26e83066f5eb48612ee47
Becky <25420142+beckvalle@users.noreply.github.com>
2021-08-05 10:29:29 -0500

    Update runjobs.sh

--- runjobs.sh
+++ runjobs.sh
@@ -1,4 +1,4 @@
 #!/bin/tcsh
 
-@ SEED=$param_start_value+$SLURM_PROCID
+@ SEED = $param_start_value + $SLURM_PROCID
 echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value, SEED: $SEED\n"
