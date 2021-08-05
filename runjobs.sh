9e2d81055f19c3bbc7311c06747332d9489cdc4c
Becky <25420142+beckvalle@users.noreply.github.com>
2021-08-05 10:13:04 -0500

    Update runjobs.sh

--- runjobs.sh
+++ runjobs.sh
@@ -1,3 +1,8 @@
 #!/bin/tcsh
 
-echo -e "This is the shell script, node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value\n"
+echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value\n"
+
+[ ! -d "/$result_folder/" ] && echo "Directory /$result_folder/ DOES NOT exist" 
+[ -d "/$result_folder/" ] && echo "Directory /$result_folder/ DOES exist" 
+
+# python run_fireabm.py -nv 10 -sd $seed -epath $out_path -ofd demo_quick_start -strat dist -rg Sta_Rosa_2000.pkl -exdsc 'demo_run' -strd 1.0
