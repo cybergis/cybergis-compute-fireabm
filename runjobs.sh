97dd7d843b5634e9fec850e604da63ab37b97921
Becky <25420142+beckvalle@users.noreply.github.com>
2021-08-05 10:50:38 -0500

    Update runjobs.sh

--- runjobs.sh
+++ runjobs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pip install ipython
-
 SEED=$(($param_start_value + $SLURM_PROCID))
 echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value, SEED: $SEED\n"
 
