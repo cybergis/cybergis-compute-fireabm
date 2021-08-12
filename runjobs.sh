bec2d19a7c8e1ebc492511a754182903e835dd7c
Becky <25420142+beckvalle@users.noreply.github.com>
2021-08-12 18:49:34 -0500

    Update runjobs.sh

--- runjobs.sh
+++ runjobs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SEED=$(($param_start_value + $SLURM_PROCID))
-echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value, SEED: $SEED\n"
-mkdir demo_quick_start
+echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value, SEED: $SEED, result folder: $result_folder\n"
+mkdir demo_quick_start$SEED
+ls -l
 MPLBACKEND=Agg python run_fireabm.py -nv 10 -sd $SEED -epath $result_folder -ofd demo_quick_start -strat dist -rg Sta_Rosa_2000.pkl -exdsc 'demo_run' -strd 1.0
