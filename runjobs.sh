60295461b134476401a02d5bf5c86a000138fe0d
Becky <25420142+beckvalle@users.noreply.github.com>
2021-08-12 19:09:53 -0500

    Update runjobs.sh

--- runjobs.sh
+++ runjobs.sh
@@ -2,6 +2,7 @@
 
 SEED=$(($param_start_value + $SLURM_PROCID))
 echo -e "node id: $SLURM_NODEID, task id: $SLURM_PROCID, start number: $param_start_value, SEED: $SEED, result folder: $result_folder\n"
+pwd
 mkdir demo_quick_start$SEED
 chmod 755 demo_quick_start$SEED
 ls -l
